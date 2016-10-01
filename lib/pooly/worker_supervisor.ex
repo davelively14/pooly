d8d45509a5ef486b0277c0491f1b8be8082aab20
Dave Lively <davelively14@gmail.com>
2016-10-01 09:04:43 -0400

    Added docs

--- lib/pooly/worker_supervisor.ex
+++ lib/pooly/worker_supervisor.ex
@@ -16,15 +16,21 @@ defmodule Pooly.WorkerSupervisor do
   #############
 
   def init({m,f,a}) do
-    # Specifies that the worker is always to be restarted and passes the
-    # function that starts the worker
+    # Specifies that the worker is always to be restarted (:permanent) and
+    # passes the function, f, that starts the worker.
     worker_opts = [restart: :permanent, function: f]
 
-    # Creates a list of the child processes
+    # Creates a list of the child processes. The worker function creates a child
+    # specification.
     children = [worker(m, a, worker_opts)]
 
+    # The simple_one_for_one strategy allows for dynamic building of children
+    # based on a single specification. The max_restarts indicates the maximum
+    # number of restarts in max_seconds amount of time before termination. By
+    # default, this is set to max_restarts: 3, max_seconds: 5
     opts = [strategy: :simple_one_for_one, max_restarts: 5, max_seconds: 5]
 
+    # The return value must be a supervisor specification
     supervise(children, opts)
   end
 end
