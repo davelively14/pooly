ea0378d4d8c05cd8be5700127553cfa61221468f
Dave Lively <davelively14@gmail.com>
2016-10-04 14:32:12 -0400

    Added documentation

--- lib/pooly.ex
+++ lib/pooly.ex
@@ -1,6 +1,7 @@
 defmodule Pooly do
   use Application
 
+  # Called by default when included in mix.exs.
   def start(_type, _args) do
     pool_config = [mfa: {SampleWorker, :start_link, []}, size: 5]
     start_pool(pool_config)
@@ -9,6 +10,9 @@ defmodule Pooly do
   def start_pool(pool_config) do
     Pooly.Supervisor.start_link(pool_config)
   end
+
+  # Below is a series of functions that allow the user to interact with the
+  # server without having to call Pooly.Server every time.
 
   def checkout do
     Pooly.Server.checkout
