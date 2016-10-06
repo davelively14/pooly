0b6201ca7a83697dcc1cdfe4eda0984f3e249aae
Dave Lively <davelively14@gmail.com>
2016-10-05 20:49:25 -0400

    Created PoolsSupervisor

---
+++ lib/pooly/pools_supervisor.ex
@@ -0,0 +1,27 @@
+defmodule Pooly.PoolsSupervisor do
+  use Supervisor
+
+  #######
+  # API #
+  #######
+
+  # Starts the PoolsSupervisor and makes it a self-named process
+  def start_link do
+    Supervisor.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  #############
+  # Callbacks #
+  #############
+
+  # Supervisor is intially started with no pools attached. We will instead
+  # validate the pool configuration before creating any pools. As a result, we
+  # only supply the restart strategy.
+  def init(_) do
+
+    # Ensure that a crash in one pool doesn't affect any other pool.
+    opts = [strategy: :one_for_one]
+
+    supervise([], opts)
+  end
+end
