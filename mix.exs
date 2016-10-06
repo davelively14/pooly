caa472056f04f367d275faac73ee53efe257b73e
Dave Lively <davelively14@gmail.com>
2016-10-06 15:25:39 -0400

    Updated version number

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Pooly.Mixfile do
 
   def project do
     [app: :pooly,
-     version: "0.0.3",
+     version: "0.0.4",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
