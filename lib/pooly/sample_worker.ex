04f41931df2c283ff95873ff621f17b0061d25c8
Dave Lively <davelively14@gmail.com>
2016-10-09 10:55:01 -0400

    Added work_for functionality

--- lib/pooly/sample_worker.ex
+++ lib/pooly/sample_worker.ex
@@ -1,6 +1,10 @@
 defmodule SampleWorker do
   use GenServer
 
+  #######
+  # API #
+  #######
+
   def start_link(_) do
     GenServer.start_link(__MODULE__, :ok, [])
   end
@@ -9,7 +13,22 @@ defmodule SampleWorker do
     GenServer.call(pid, :stop)
   end
 
+  # Tells process to stay alive for a designated amount of time
+  def work_for(pid, duration) do
+    GenServer.cast(pid, {:work_for, duration})
+  end
+
+  #############
+  # Callbacks #
+  #############
+
   def handle_call(:stop, _from, state) do
     {:stop, :normal, :ok, state}
+  end
+
+  # Sleeps for a designated amount of time, then terminates normally.
+  def handle_cast({:work_for, duration}, state) do
+    :timer.sleep(duration)
+    {:stop, :normal, state}
   end
 end
